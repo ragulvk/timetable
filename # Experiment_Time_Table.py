1f1adf1c1164abf8549deec711026e7d1c88b1c0
ragulvk <ragulvk12@gmail.com>
2021-12-03 00:48:00 -0800

    timttable

---
+++ # Experiment_Time_Table.py
@@ -0,0 +1,92 @@
+# Experiment_Time_Table
+
+## AIM
+To Write a html webpage page to display your timetable.
+
+# ALGORITHM
+### STEP 1
+create a simple table using table tag
+### STEP 2
+Add header row using th tag
+### STEP 3
+Add your timetable
+### STEP 4
+Execute the program
+
+# CODE
+~~~<!DOCTYPE html>
+<html>
+ <head>
+ <title>TIME TABLE</title>
+ </head>
+ <body>
+ <table border = "1" cellspacing="1" bordercolor="blue" bgcolor="yellow">
+ <tr>
+ <th colspan="8">TIME TABLE</th>
+ </tr>
+<tr>
+<th>reference number</th>
+<th>21003061</th>
+ <tr>
+ <th>DAYS</th>
+ <th>1</th>
+ <th>2</th>
+ <th>3</th>
+ <th>4</th>
+ <th rowspan="6">lunch break</th>
+ <th>5</th>
+ <th>6</th>
+ </tr>
+
+<tr>
+<th>MONDAY</th>
+<th>fundamentals of web technology</th>
+<th>fundamentals of web technology</th>
+<th>linear algebra laboratory</th>
+<th>linear algebra laboratory</th>
+<th>mathematics for artificial inteligence</th>
+<th>mathematics for artificial inteligence</th>
+</tr>
+<tr>
+<th>TUESDAY</th>
+<td align="center">-</td>
+<td align="center">-</td>
+<th>engineeing mechanics and product development</th>
+<th>engineeing mechanics and product development</th>
+<th>engineering desigin and modeling</th>
+<th>engineering desigin and modeling</th>
+<tr>
+<th>WEDNESDAY</th>
+<td align="center">-</td>
+<td align="center">-</td>
+<th>mathematics for artificial inteligence</th>
+<th>mathematics for artificial inteligence</th>
+<th>fundamentals of web technology</th>
+<th>fundamentals of web technology</th>
+<tr>
+<th>THURSDAY</th>
+<th>engineering desigin and modeling</th>
+<th>engineering desigin and modeling</th>
+<th>python programming</th>
+<th>python programming</th>
+<th>engineeing mechanics and product development</th>
+<th>engineeing mechanics and product development</th>
+<tr>
+<th>FRIDAY</th>
+<th>soft skills</th>
+<th>soft skills</th>
+<th>python programming</th>
+<th>python programming</th>
+<th>web technology laboratory</th>
+<th>web technology laboratory</th>
+<tr>
+</body>
+</html>
+
+~~~
+## OUTPUT
+![Github logo](img.png)
+
+## VALIDATION RESULT:
+![github logo](timetable.png)
+
